0a9ebab95fa002d7501facdc2ee4b58666d02340
Yan Valavik <yan.volovik@gmail.com>
2024-11-14 21:30:08 +0100

    Deploy error fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,8 @@ import { OrderModule } from './order/order.module';
 import { ReviewModule } from './review/review.module';
 import { AdminModule } from './admin/admin.module';
 import { SequelizeModule } from '@nestjs/sequelize';
+import { User } from './user/user.model';
+import { Vinyl } from './vinyl/vinyl.model';
 import { EmailService } from './email/email.service';
 import { AuthModule } from './auth/auth.module';
 import { EmailModule } from './email/email.module';
@@ -14,30 +16,22 @@ import { ConfigModule } from './config/config.module';
 import { RolesGuard } from './auth/roles.guard';
 import { VinylService } from './vinyl/vinyl.service';
 import { APP_GUARD } from '@nestjs/core';
-import { ConfigService } from './config/config.service';
 import { Review } from './review/review.model';
-import { User } from './user/user.model';
-import { Vinyl } from './vinyl/vinyl.model';
 
 @Module({
   imports: [
-    ConfigModule,
-    SequelizeModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: async (configService: ConfigService) => ({
-        dialect: 'mysql',
-        url: configService.get<string>('MYSQL_PUBLIC_URL'),
-        host: configService.get<string>('MYSQLHOST'),
-        port: configService.get<number>('MYSQLPORT', 3306),
-        username: configService.get<string>('MYSQLUSER'),
-        password: configService.get<string>('MYSQL_ROOT_PASSWORD'),
-        database: configService.get<string>('MYSQL_DATABASE'),
-        models: [User, Vinyl, Review],
-        autoLoadModels: true,
-        synchronize: true,
-      }),
+    SequelizeModule.forRoot({
+      dialect: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'my_user',
+      password: 'password',
+      database: 'my_project_db',
+      models: [User, Vinyl, Review],
+      autoLoadModels: true,
+      synchronize: true,
     }),
+    ConfigModule,
     UserModule,
     VinylModule,
     OrderModule,
